d84f77e92d87e7bf22157aaf983d71df7c0a891f
ungeism <shoichi.tokuyama@holo-bell.com>
2022-03-21 00:40:49 +0900

    v1.0

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    // browserを立ち上げる
     const browser = await puppeteer.launch();
-    // page(tab)を生成する
     const page = await browser.newPage();
-    // 特定のURLへ移動する｡
     await page.goto('https://google.com');
-    // スクリーンショット作成
     await page.screenshot({ path: 'img/sample.png' });
-    // browserを閉じる
     await browser.close();
 })();
